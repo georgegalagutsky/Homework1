7cb98bf161bb1d41d6cec16c36c4001f5ae3f0be
George Galagutsky <georgegalagutsky@gmail.com>
2024-02-20 17:44:46 +0300

    Task1

---
+++ src/main/Task1/Task1.java
@@ -0,0 +1,13 @@
+public class Task1 {
+        public static void main(String[] args) {
+
+        int b = 1; int c = 2;
+        int a = 4*(b+c-1)/2;
+
+        System.out.println(a);
+    }
+}
+/*Задача 1:
+Написать приложение, которое будет вычислять и выводить значение по формуле:
+a=4*(b+c-1)/2; b и c задаем в коде самостоятельно.*/
+
